a6d368da9084e6e137ab8b529fcb1fd284cf80a3
gitting-it-right <sreekar.garlapati@gmail.com>
2019-04-02 17:58:44 +0100

    modified files in bp_test, need to debug step_train

---
+++ bp_test/bp_test_main.cpp
@@ -0,0 +1,96 @@
+#include <spdlog/spdlog.h>
+#include <stdlib.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "matrix.hpp"
+#include "nn.hpp"
+
+std::vector<std::string> split(const std::string& s, char delimiter)
+{
+   std::vector<std::string> tokens;
+   std::string token;
+   std::istringstream tokenStream(s);
+   while (std::getline(tokenStream, token, delimiter))
+   {
+      tokens.push_back(token);
+   }
+   return tokens;
+}
+// https://www.fluentcpp.com/2017/04/21/how-to-split-a-string-in-c/
+
+Matrix getTarget(const std::string& s){
+  int out_class = -1;
+
+  if(s == "Iris-setosa"){
+    out_class = 0;
+  }
+  else if(s == "Iris-versicolor"){
+    out_class = 1;
+  }
+  else if(s == "Iris-virginica"){
+    out_class = 2;
+  }
+  else{
+    throw "unidentified class";
+  }
+  Matrix result = Matrix(3,1,"target");
+  result.setZeros();
+  result.set(out_class,0,1);
+  return result;
+}
+std::vector<std::string> outputs = {"Iris-setosa","Iris-versicolor","Iris-virginica"};
+
+int main() {
+  srand(42);
+  spdlog::set_level(spdlog::level::debug);
+  spdlog::set_pattern("[%^%L%$][%t][%H:%M:%S.%f] %v");
+
+  try {
+    auto fnn = FullyConnectedNetwork();
+    fnn.addLayer(4);
+    fnn.addLayer(10);
+    fnn.addLayer(10);
+    fnn.addLayer(3);
+    fnn.compile();
+    float lr = 0.001;
+    
+    std::ifstream datastream; 
+    datastream.open("iris_data.txt"); 
+    std::string line;
+    while(std::getline(datastream,line)){
+      std::vector<std::string> tokens = split(line,',');
+      Matrix target = getTarget(tokens[tokens.size()-1]);
+      tokens.povirginicap_back();
+      Matrix input(tokens.size(),1,"input");
+      for(int i = 0; i < tokens.size(); i++){
+        input.set(i,0,std::stof(tokens[i]));
+      }
+      std::cout<<"a\n";
+      fnn.step_train(input,target,lr);
+      std::cout<<"B\n";
+    }
+    datastream.close();
+
+    
+    std::ifstream testdata; 
+    testdata.open("iris_test.txt"); 
+    while(std::getline(datastream,line)){
+      std::vector<std::string> tokens = split(line,',');
+      // Matrix target = getTarget(tokens[tokens.size()-1]);
+      std::string actual = tokens[tokens.size()-1];
+      tokens.pop_back();
+      Matrix input(tokens.size(),1,"input");
+      for(int i = 0; i < tokens.size(); i++){
+        input.set(i,0,std::stof(tokens[i]));
+      }
+      std::cout<<"Prediction : "<<outputs[fnn.predict(input)]<<" Actual : "<<actual<<std::endl;
+    }
+    
+    
+  } catch (std::string e) {
+    spdlog::error(e);
+  }
+
+  return 0;
+}
---
+++ bp_test/nn.hpp
@@ -0,0 +1,124 @@
+#ifndef NN_HPP
+#define NN_HPP
+
+#include "matrix.hpp"
+#include <vector>
+
+class FullyConnectedNetwork {
+  std::vector<int> layerDims;
+  std::vector<Matrix> weights;
+  std::vector<Matrix> biases;
+  bool isCompiled;
+
+  public:
+
+  FullyConnectedNetwork() : isCompiled(false) {}
+
+  ~FullyConnectedNetwork() {}
+
+  void addLayer(int layerDim) {
+    layerDims.push_back(layerDim);
+  }
+
+  void compile() {
+    if (layerDims.size() <= 1) {
+      std::stringstream ss;
+      ss <<  "Attempt to compile a neural net with <= 1 layers";
+      throw ss.str();
+    }
+
+    if (isCompiled) {
+      spdlog::warn("Attempt to compile neural net multiple times!");
+      return;
+    }
+
+    int numTrainableParams = 0;
+    for(int i = 0; i < layerDims.size() - 1; i++) {
+      weights.push_back(Matrix(layerDims[i], layerDims[i + 1]));
+      weights[i].setUniform(-1, 1);
+
+      biases.push_back(Matrix(1, layerDims[i + 1]));
+      biases[i].setZeros();
+
+      numTrainableParams += weights[i].getNumElements() + biases[i].getNumElements();
+    }
+    spdlog::info("Total number of trainable parameters : {}", numTrainableParams);
+
+    for(int i = 0; i < weights.size(); i++) {
+      std::stringstream name;
+      name << "weight_" << i;
+      weights[i].setName(name.str());
+    }
+
+    for(int i = 0; i < biases.size(); i++) {
+      std::stringstream name;
+      name << "bias_" << i;
+      biases[i].setName(name.str());
+    }
+
+    isCompiled = true;
+  }
+
+  Matrix forwardPass(const Matrix& inputMatrix) {
+    Matrix* in = new Matrix(inputMatrix);
+    for(int i = 0; i < weights.size(); i++) {
+      Matrix out = (*in * weights[i] + biases[i]).sigmoid();
+      delete in;
+      in = new Matrix(out);
+    }
+    Matrix result = *in;
+    delete in;
+    return result;
+  }
+
+  void step_train(const Matrix& inputMatrix, const Matrix& targetMatrix,float alpha){
+    std::vector<Matrix*> ins,outs;
+    Matrix* in = new Matrix(inputMatrix);
+    Matrix target = Matrix(targetMatrix);
+    outs.push_back(in);
+    // FORWARD PASS
+    for(int i = 0; i < weights.size(); i++) {
+      Matrix* in = new Matrix(*in * weights[i] + biases[i]);
+      Matrix* out = new Matrix(in->sigmoid());
+      ins.push_back(in);
+      outs.push_back(out);
+      in = out;
+    }
+    // BACKPROP
+    int n = weights.size();
+    Matrix* delta;
+    for(int i = n - 1 ; i >= 0; i--){
+      if(i == n - 1){
+          delta = new Matrix((target - *outs[i+1]) % *ins[i]);
+          Matrix change = (*outs[i]) * (~*delta);
+          weights[i] = weights[i] + change * alpha;
+      }
+      else{
+        Matrix* delta2 = new Matrix( (ins[i]->delsigmoid()) % (weights[i+1] * *delta) );
+        delete delta;
+        delta = delta2;
+        Matrix change = (*outs[i]) * (~*delta);
+        weights[i] = weights[i] + change * alpha;
+      }
+    }
+    for(auto it : ins){
+      delete it;
+    }
+    for(auto it : outs){
+      delete it;
+    }
+  }
+  int predict(const Matrix& inputMatrix){
+    Matrix res = forwardPass(inputMatrix);
+    int maxr = -1;
+    float maxval = -1000;
+    for(int i = 0; i < res.nR; i++){
+      if(res.at(i,0) > maxval){
+        maxr = i,maxval = res.at(i,0);
+      }
+    }
+    return maxr;
+  }
+};
+
+#endif

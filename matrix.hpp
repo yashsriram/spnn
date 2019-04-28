803accc092b3f68407e3068c2545dc609cbfff83
gitting-it-right <sreekar.garlapati@gmail.com>
2019-04-28 21:55:36 +0100

    removed 2d vector, replaced with 1d vector

--- matrix.hpp
+++ matrix.hpp
@@ -9,10 +9,11 @@
 #include <algorithm>
 #include <utility>
 
-bool USE_MATRIX_NAMES = true;
+bool USE_MATRIX_NAMES = false;
 
 class Matrix {
-  std::vector<std::vector<float> > values;
+  // std::vector<std::vector<float> > values;
+  std::vector<float> values;
   friend std::ostream& operator<<(std::ostream&, const Matrix&);
 
 public:
@@ -21,22 +22,16 @@ public:
 
   Matrix(int r, int c, std::string name = USE_MATRIX_NAMES ? "<unnamed-matrix>" : ""): nR(r), nC(c), name(name) {
     spdlog::debug("Matrix {}: constructor called", name.c_str());
-    values.resize(nR);
-    for (auto& row: values) {
-      row.resize(nC);
-    }
+    values.resize(nR*nC);
   }
 
   Matrix(const Matrix& m) : nR(m.nR), nC(m.nC), name(USE_MATRIX_NAMES ? "(" + m.name + ")_copy" : "") {
     spdlog::debug("Matrix {}: copy constructor called", name.c_str());
-    values.resize(nR);
-    for (auto& row: values) {
-      row.resize(nC);
-    }
+    values.resize(nR*nC);
     // deep copy the values variable
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        values[i][j] = m.values[i][j];
+        values[i*nC+j] = m.values[i*nC+j];
       }
     }
   }
@@ -56,7 +51,7 @@ public:
     name = USE_MATRIX_NAMES ? "(" + m.name + ")_copy" : "";
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        values[i][j] = m.values[i][j];
+        values[i*nC+j] = m.values[i*nC+j];
       }
     }
   }
@@ -70,7 +65,7 @@ public:
   void printDims() const { spdlog::info("nR = {}, nC = {}",nR,nC); }
 
   float get(const int& i, const int& j) const {
-    return values[i][j];
+    return values[i*nC+j];
   }
 
   float get(const std::pair<int, int>& index) const {
@@ -78,32 +73,21 @@ public:
   }
 
   float& at(const int& i, const int& j) {
-    return values[i][j];
+    return values[i*nC+j];
   }
 
   std::pair<int, int> argmax() const {
-    std::vector<int> argmaxCols;
-    std::vector<float> rowwiseMaxs;
-    for (int i = 0; i < nR; ++i) {
-      auto row = values[i];
-      int argmaxCol = std::max_element(row.begin(), row.end()) - row.begin();
-      float maxInRow = *std::max_element(row.begin(), row.end());
-      argmaxCols.push_back(argmaxCol);
-      rowwiseMaxs.push_back(maxInRow);
-    }
-    int argmaxRow = std::max_element(rowwiseMaxs.begin(), rowwiseMaxs.end()) - rowwiseMaxs.begin();
-    int argmaxCol = argmaxCols[argmaxRow];
-
-    return std::pair<int, int>(argmaxRow, argmaxCol);
+    auto max_index = std::max_element(values.begin(),values.end()) - values.begin();
+    return std::make_pair(max_index/nC, max_index%nC);
   }
 
   std::pair<int, int> colmax(int col) const {
-    float ans = -10000000;
+    float ans = -10000000; // CHANGE IN FUTURE
     int max_ind = -1;
     for (int i = 0; i < nR; ++i) {
-      if(values[i][col] > ans){
+      if(values[i*nC+col] > ans){
         max_ind = i;
-        ans = values[i][col];
+        ans = values[i*nC+col];
       }
     }
     return std::pair<int, int>(max_ind, col);
@@ -112,7 +96,7 @@ public:
   Matrix* setZeros() {
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        values[i][j] = 0;
+        values[i*nC+j] = 0;
       }
     }
     return this;
@@ -121,7 +105,7 @@ public:
   Matrix* setOnes() {
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        values[i][j] = 1;
+        values[i*nC+j] = 1;
       }
     }
     return this;
@@ -130,7 +114,7 @@ public:
   Matrix* setIdentity() {
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        values[i][j] = (i == j);
+        values[i*nC+j] = (i == j);
       }
     }
     return this;
@@ -141,7 +125,7 @@ public:
       for (int j = 0; j < nC; ++j) {
         float randomNumber = (float) rand() / RAND_MAX;
         randomNumber = low + randomNumber * (high - low);
-        values[i][j] = randomNumber;
+        values[i*nC+j] = randomNumber;
       }
     }
     return this;
@@ -154,7 +138,7 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[i][j] = 1 / (1 + exp(-this->values[i][j]));
+        result.values[i*nC+j] = 1 / (1 + exp(-this->values[i*nC+j]));
       }
     }
 
@@ -168,8 +152,8 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[i][j] = 1 / (1 + exp(-this->values[i][j]));
-        result.values[i][j] = result.values[i][j] - (result.values[i][j]*result.values[i][j]);
+        result.values[i*nC+j] = 1 / (1 + exp(-this->values[i*nC+j]));
+        result.values[i*nC+j] = result.values[i*nC+j] - (result.values[i*nC+j]*result.values[i*nC+j]);
       }
     }
 
@@ -184,10 +168,10 @@ public:
     for (int j = 0; j < nC; ++j) {
       float sum = 0;
       for (int i = 0; i < nR; ++i) {
-        sum += exp(this->values[i][j]);
+        sum += exp(this->values[i*nC+j]);
       }
       for (int i = 0; i < nR; ++i) {
-        result.values[i][j] = exp(this->values[i][j]) / sum;
+        result.values[i*nC+j] = exp(this->values[i*nC+j]) / sum;
       }
     }
 
@@ -201,7 +185,7 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[j][i] = this->values[i][j];
+        result.values[j*nR+i] = this->values[i*nC+j];
       }
     }
 
@@ -216,7 +200,7 @@ public:
 
       for (int i = 0; i < nR; ++i) {
         for (int j = 0; j < nC; ++j) {
-          result.values[i][j] = this->values[i][j] + m.values[0][j];
+          result.values[i*nC+j] = this->values[i*nC+j] + m.values[j];
         }
       }
       return result;
@@ -238,7 +222,7 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[i][j] = this->values[i][j] + m.values[i][j];
+        result.values[i*nC+j] = this->values[i*nC+j] + m.values[i*nC+j];
       }
     }
 
@@ -261,7 +245,7 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[i][j] = this->values[i][j] - m.values[i][j];
+        result.values[i*nC+j] = this->values[i*nC+j] - m.values[i*nC+j];
       }
     }
 
@@ -286,9 +270,9 @@ public:
       for (int j = 0; j < m.nC; ++j) {
         float elementSum = 0;
         for (int k = 0; k < nC; ++k) {
-          elementSum += this->values[i][k] * m.values[k][j];
+          elementSum += this->values[i*nC+k] * m.values[k*m.nC+j];
         }
-        result.values[i][j] = elementSum;
+        result.values[i*m.nC+j] = elementSum;
       }
     }
 
@@ -302,7 +286,7 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[i][j] = this->values[i][j] * value;
+        result.values[i*nC+j] = this->values[i*nC+j] * value;
       }
     }
 
@@ -311,19 +295,19 @@ public:
 
 
   Matrix operator%(Matrix const &m) const {
-    if (m.nC == 1 && nR == m.nR) {
-      std::stringstream ss;
-      ss << name << " % " << m.name;
-      Matrix result(nR, nC, USE_MATRIX_NAMES ? ss.str() : "");
+    // if (m.nC == 1 && nR == m.nR) {
+    //   std::stringstream ss;
+    //   ss << name << " % " << m.name;
+    //   Matrix result(nR, nC, USE_MATRIX_NAMES ? ss.str() : "");
 
-      for (int i = 0; i < nR; ++i) {
-        for (int j = 0; j < nC; ++j) {
-          result.values[i][j] = this->values[i][j] * m.values[i][0];
-        }
-      }
+    //   for (int i = 0; i < nR; ++i) {
+    //     for (int j = 0; j < nC; ++j) {
+    //       result.values[i][j] = this->values[i][j] * m.values[i][0];
+    //     }
+    //   }
 
-      return result;
-    }
+    //   return result;
+    // }
 
     if (nC != m.nC || nR != m.nR) {
       std::stringstream ss;
@@ -340,7 +324,7 @@ public:
 
     for (int i = 0; i < nR; ++i) {
       for (int j = 0; j < nC; ++j) {
-        result.values[i][j] = this->values[i][j] * m.values[i][j];
+        result.values[i*nC+j] = this->values[i*nC+j] * m.values[i*nC+j];
       }
     }
 
@@ -353,7 +337,7 @@ std::ostream& operator<<(std::ostream &out, const Matrix &m) {
   out << m.name << " of shape: (" << m.nR << "," << m.nC << ") is\n";
   for (int i = 0; i < m.nR; ++i) {
     for (int j = 0; j < m.nC; ++j) {
-      out << m.values[i][j] << " ";
+      out << m.values[i*m.nC+j] << " ";
     }
     out << std::endl;
   }

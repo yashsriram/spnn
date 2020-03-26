48becbf1355b94b634f8d31990d38a9effe75470
Yashasvi Sriram <yash.3997@gmail.com>
2020-03-26 01:46:05 -0500

    updates makefile

--- data/iris/shuffle.py
+++
@@ -1,9 +0,0 @@
-
-from random import shuffle
-with open('iris_data.txt') as f:
-    content = f.readlines()
-shuffle(content)
-with open('iris_data.txt','w') as f:
-    for line in content:
-        f.write(line)
-
